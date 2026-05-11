1116499060c2166b54cf4242fd87877b45d0757b
yooseohyeon <yooseohyeon.02@gmail.com>
2026-05-11 15:20:36 +0900

    feat: 실습4 - 전역 QueryClient의 staleTime 변경

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,17 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 10,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
